a9f9348ce16c53ebc13facbc0b5692b141cb2a86
shiftleino <shiftleino@gmail.com>
2023-10-27 19:06:09 +0300

    Upload raw files to GCP

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Living area mapper
 The application finds the most similar living areas for the postal code areas of the six largest municipalities in Finland.
 
+Link to the application: https://lookerstudio.google.com/reporting/3dbccbd5-a076-4ee1-b2c3-674714229193/page/ugXgD
+
 ## Source data
 - https://pxdata.stat.fi/PxWeb/pxweb/fi/StatFin/
 - https://pxdata.stat.fi/PXWeb/pxweb/fi/Postinumeroalueittainen_avoin_tieto
--- data-extraction/src/statsfin-data-extractor/__main__.py
+++ data-extraction/src/statsfin-data-extractor/__main__.py
@@ -1,3 +1,4 @@
+from google.cloud import storage
 import logging
 import tomllib
 import os
@@ -29,11 +30,21 @@ def main():
     apartment_prices_areas = extract_apartment_price_info_for_areas(config["sources"]["apartment_prices_area_url"], str(current_year - 1))
     apartment_prices_municipalities = extract_apartment_price_info_for_municipalities(config["sources"]["apartment_prices_municipality_url"], str(current_year - 1))
 
-    postal_code_info_latest.to_csv("../data/postal_code_info_latest.csv", index=True, sep=";", index_label="Postal code")
-    postal_code_info_old.to_csv("../data/postal_code_info_old.csv", index=True, sep=";", index_label="Postal code")
-    apartment_prices_areas.to_csv("../data/apartment_prices_areas.csv", index=True, sep=";", index_label="Postal code")
-    apartment_prices_municipalities.to_csv("../data/apartment_prices_municipalities.csv", index=True, sep=";")
-    postal_code_mapping.to_csv("../data/postal_code_mapping.csv", index=False, sep=";")
+    logging.info("Creating client for authenticating to GCP")
+    storage_client = storage.Client(project=config["cloud"]["project_id"])
+    bucket = storage_client.bucket(config["cloud"]["bucket_name"])
+    postal_code_info_latest_blob = bucket.blob("postal_code_info_latest.csv")
+    postal_code_info_old_blob = bucket.blob("postal_code_info_old.csv")
+    apartment_prices_areas_blob = bucket.blob("apartment_prices_areas.csv")
+    apartment_prices_municipalities_blob = bucket.blob("apartment_prices_municipalities.csv")
+    postal_code_mapping_blob = bucket.blob("postal_code_mapping.csv")
+    
+    logging.info("Uploading the dataframes as csv-files to GCP cloud storage bucket")
+    postal_code_info_latest_blob.upload_from_string(postal_code_info_latest.to_csv(index=True, sep=";", index_label="Postal code"))
+    postal_code_info_old_blob.upload_from_string(postal_code_info_old.to_csv(index=True, sep=";", index_label="Postal code"))
+    apartment_prices_areas_blob.upload_from_string(apartment_prices_areas.to_csv(index=True, sep=";", index_label="Postal code"))
+    apartment_prices_municipalities_blob.upload_from_string(apartment_prices_municipalities.to_csv(index=True, sep=";"))
+    postal_code_mapping_blob.upload_from_string(postal_code_mapping.to_csv(index=False, sep=";"))
 
 
 if __name__ == "__main__":
